92b264408122ba12c8ecdf4c08973ce9b5612007
monikj2004 <monikjayagopi@gmail.com>
2025-01-03 11:09:12 +0530

    operaters.py
    operaters.py

---
+++ else.py
@@ -0,0 +1,23 @@
+# Simple Python Program to print the largest of the three numbers.  
+a = int (input("Enter a: "));    
+b = int (input("Enter b: "));    
+c = int (input("Enter c: "));    
+if a>b and a>c:    
+# Here, we are checking the condition. If the condition is true, we will enter the block  
+    print ("From the above three numbers given a is largest");    
+if b>a and b>c:    
+# Here, we are checking the condition. If the condition is true, we will enter the block  
+    print ("From the above three numbers given b is largest");    
+if c>a and c>b:    
+# Here, we are checking the condition. If the condition is true, we will enter the block  
+    print ("From the above three numbers given c is largest");    
+
+
+# Simple Python Program to check whether a person is eligible to vote or not.  
+age = int (input("Enter your age: "))    
+# Here, we are taking an integer num and taking input dynamically  
+if age>=18:    
+# Here, we are checking the condition. If the condition is true, we will enter the block  
+    print("You are eligible to vote !!");    
+else:    
+    print("Sorry! you have to wait !!");    
---
+++ operaters.py
@@ -0,0 +1,21 @@
+a=7
+b=8
+print("The addition of two numers", a+b)
+#addition
+print("The subraction of two numbers", a-b)
+#subraction
+print("The multiplication of two numbers", a*b)
+#multiplication
+print( "The division of two numbers ",a/b)
+#division
+
+sum=a+b
+#addition
+sub=a-b
+#subraction
+mul=a*b
+#multiplication
+div=a/b
+#division
+
+
